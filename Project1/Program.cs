4de16651ed3021688765bd92b3c688fa110ce892
DIRSON <dgutsal221@gmail.com>
2023-07-27 21:39:59 +1000

    Добавьте файлы проекта.

---
+++ Project1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            int programCount;
+            int money;
+            int time;
+            bool truth;
+
+            name ="Денис";
+            age = 22;
+            programCount = 1;
+            money = 100000;
+            time = 604800;
+            truth = true;
+
+            Console.WriteLine("Здравствйте меня зовут " + name + ".");
+            Console.WriteLine("Мне " + age + " лет.");
+            Console.WriteLine("Я хочу научиться делать игры, это моя " + programCount + " программа на языке C#");
+            Console.WriteLine("Когда я закончу обучения я хочу зарабатывать " + money + " денег в " + time + " наносикунд");
+            Console.WriteLine("Достигну ли я желаемого? " + truth + "!");
+
+        }
+    }
+}
